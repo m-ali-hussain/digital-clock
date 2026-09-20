1fa8f01e7085fe3dda06ce403495454290d3843f
Muhammad Ali Hussain <ali6351285@gmail.com>
2026-09-20 17:55:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Digital Clock
+
+function updateClock(){
+
+    const now = new Date();
+    let hours = now.getHours()
+    const meridium = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    const formattedHours = hours.toString().padStart(2,"0");
+    const minutes = now.getMinutes().toString().padStart(2,"0");
+    const seconds = now.getSeconds().toString().padStart(2,"0");
+    const timeString = `${formattedHours}:${minutes}:${seconds}:${meridium}`;
+    document.getElementById("clock").textContent = timeString;
+}
+updateClock();
+setInterval(updateClock, 1000)
